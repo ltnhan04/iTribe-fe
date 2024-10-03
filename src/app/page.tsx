c2ebc8ca6ff966857b3b4a80feb77ce9d25e8576
Luong Tu Nhan <tunhan281204@gmail.com>
2024-10-03 17:45:29 +0700

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Footer from "@/components/common/footer";
+import Navbar from "@/components/common/navbar";
+import Hero from "@/components/common/hero";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="container max-w-7xl mx-auto sm:px-5">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/components/common/footer.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { footerLinks } from "@/constants/page";
+
+export default function Footer() {
+  return (
+    <div className="w-full py-5 px-5 sm:px-10">
+      <div className="flex items-center justify-between">
+        <Image
+          src={"/images/iTribe-logo.png"}
+          width={64}
+          height={64}
+          className="cursor-pointer w-12 h-12 sm:h-16 sm:w-16"
+          priority={true}
+          alt="iTribe logo"
+          quality={100}
+        />
+        <div>
+          <div className="flex items-center justify-center">
+            {footerLinks.map((link, index) => (
+              <Link
+                href={"https://github.com/ltnhan04/iTribe-fe"}
+                target="_blank"
+                key={index}
+                className="font-semibold text-gray text-xs hover:underline hover:text-gray-200"
+              >
+                {link}{" "}
+                {index !== footerLinks.length - 1 && (
+                  <span className="mx-2">|</span>
+                )}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="bg-neutral-700 h-[1px] my-5 w-full"></div>
+      <p className="font-semibold text-gray text-xs text-center">
+        Copright @ 2024 iTribe Team. All rights reserved.
+      </p>
+    </div>
+  );
+}
