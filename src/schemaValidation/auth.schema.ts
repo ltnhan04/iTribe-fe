36f2a5b416529f7ac9d490765e8aac09a575b632
Luong Tu Nhan <tunhan281204@gmail.com>
2024-10-09 21:36:04 +0700

    feat: validate with Zod

---
+++ src/schemaValidation/auth.schema.ts
@@ -0,0 +1,55 @@
+import z from "zod";
+
+export const RegisterBody = z
+  .object({
+    name: z
+      .string()
+      .min(1, "Tên đăng ký phải có ít nhất 1 kí tự!")
+      .max(30, "Tên đăng ký không được vượt quá 30 kí tự!")
+      .regex(
+        /^[a-zA-Z0-9_]+$/,
+        "Tên đăng ký chỉ được chứa chữ cái, số và dấu gạch dưới!"
+      ),
+    email: z.string().email("Email không hợp lệ!"),
+    password: z
+      .string()
+      .min(8, "Mật khẩu phải có ít nhất 8 kí tự!")
+      .max(100, "Mật khẩu không được vượt quá 100 kí tự!")
+      .regex(
+        /[!@#$%^&*(),.?":{}|<>]/,
+        "Mật khẩu phải bao gồm ít nhất một ký tự đặc biệt"
+      )
+      .regex(/[A-Z]/, "Mật khẩu phải bao gồm ít nhất một ký tự hoa"),
+    confirmPassword: z
+      .string()
+      .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
+      .max(100, "Mật khẩu không được vượt quá 100 kí tự"),
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Mật khẩu không khớp!",
+        path: ["confirmPassword"],
+      });
+    }
+  });
+
+export const LoginBody = z
+  .object({
+    email: z.string().email("Email không hợp lệ!"),
+    password: z
+      .string()
+      .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
+      .max(100, "Mật khẩu không được vượt quá 100 ký tự")
+      .regex(
+        /[!@#$%^&*(),.?":{}|<>]/,
+        "Mật khẩu phải bao gồm ít nhất một ký tự đặc biệt"
+      )
+      .regex(/[A-Z]/, "Mật khẩu phải bao gồm ít nhất một ký tự hoa"),
+  })
+  .strict();
+
+export type RegisterBodyType = z.TypeOf<typeof RegisterBody>;
+export type LoginBodyType = z.TypeOf<typeof LoginBody>;
