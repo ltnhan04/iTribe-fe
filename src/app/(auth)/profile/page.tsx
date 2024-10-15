915a756d7823eae7946d8596988b0f5d31891a61
Luong Tu Nhan <tunhan281204@gmail.com>
2024-10-16 01:03:15 +0700

    feat: implement protected route

---
+++ src/app/(auth)/profile/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import withAuth from "@/components/common/withAuth";
+
+function ProfilePage() {
+  return <div>Profile</div>;
+}
+export default withAuth(ProfilePage);
