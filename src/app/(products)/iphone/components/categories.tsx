16c2dd6e24ca58131394b46cf3065b4e2c3bae3f
Luong Tu Nhan <tunhan281204@gmail.com>
2024-10-20 17:54:13 +0700

    feat: apply dynamic routes using slug

---
+++ src/app/(products)/iphone/components/categories.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { categories } from "@/constants/page";
+
+const Categories = () => {
+  const router = useRouter();
+  const bottomRef = useRef<HTMLSpanElement | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  useEffect(() => {
+    if (bottomRef.current) {
+      bottomRef.current.classList.add(
+        "border-b-2",
+        "border-blue",
+        "!text-blue"
+      );
+    }
+  }, []);
+
+  const handleSlugChange = (slug: string, index: number) => {
+    setSelectedIndex(index);
+    router.push(`/iphone/${slug}`);
+  };
+
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex items-center py-2 mt-4 gap-3 sm:gap-5">
+        {categories.map((value, index) => (
+          <span
+            key={index}
+            onClick={() => handleSlugChange(value.url, index)}
+            ref={index === 0 ? bottomRef : null}
+            className={` ${
+              index === selectedIndex
+                ? "!text-blue border-b-2 !border-blue"
+                : ""
+            } text-sm text-black py-1 font-medium transition-colors duration-300 ease-out hover:text-blue cursor-pointer border-b-2 border-transparent hover:border-blue`}
+          >
+            {value.category}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
